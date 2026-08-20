0ab42e705e5099a2dabbca5f8da68f28d9d63681
dipender98 <dipender@users.noreply.github.com>
2026-08-20 23:08:43 +0530

    fix: remove invalid pluginName/pluginClassName from cloudstream{} block (caused build failure)

--- Indflix/build.gradle.kts
+++ Indflix/build.gradle.kts
@@ -1,11 +1,6 @@
 version = 1
 
 cloudstream {
-    // The id of the plugin. This is used to uniquely identify the plugin.
-    // It must not contain any uppercase letters.
-    pluginName = "indflix"
-    // The display name of the plugin. This is shown to the user.
-    pluginClassName = "IndflixProvider"
     language = "en"
     authors = listOf("Indflix")
     description = "CloudStream provider that scrapes Multimovies and pulls sources in parallel with a per-source timeout."
